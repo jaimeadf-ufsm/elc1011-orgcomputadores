dba5dc78ea9b3814176c7cb04cde954c9a63ad39
Jaime Filho <jaimeadanielfilho@hotmail.com>
2023-09-29 20:11:55 -0300

    chore(trabalhos/t1): standardize struct defininitions and improve documentation

---
+++ trabalhos/t1/decoder.asm
@@ -0,0 +1,149 @@
+# struct <Operando>
+# 8 bytes
+#
+# Offset 0  : <word> tipo
+# Offset 4  : <word> valor
+
+# struct <Instrução Decodificada>
+# 32 bytes
+#
+# Offset 0  : <word> posição (endereço)
+# Offset 4  : <word> código de máquina
+# Offset 8  : <endereço> para <Definição da Instrução>
+# Offset 12 : <Operando> 0 
+# Offset 20 : <Operando> 1 
+# Offset 28 : <Operando> 3
+# Offset 36 : <Operando> 4 
+
+
+# Decodifica a instrução em código de máquina.
+#
+# argumentos:
+# $a0 : <endereço> para <Instrução Decodificada> em que será salvo o resultado
+# $a1 : <word> posição (endereço)
+# $a2 : <word> código de máquina
+#
+# pilha:
+# $sp + 4  : <endereço> para <Instrução Decodificada>
+# $sp + 0  : $ra
+.globl decodeinst
+decodeinst:
+    addiu $sp, $sp, -8                           # ajusta a pilha
+    sw $a0, 4($sp)                               # armazena o argumento com o <endereço> para a <Instrução Decodificada> na pilha
+    sw $ra, 0($sp)                               # armazena o endereço de retorno na pilha
+
+    sw $a1, 0($a0)                               # armazena a posição na instrução decodificada 
+    sw $a2, 4($a0)                               # armazena o código de máquina na instrução decodificada
+
+    move $a0, $a2                                # $a0 = código de máquina
+    jal lookupinst                               # busca a definição e o tipo da instrução
+
+    lw $t0, 4($sp)                               # $t0 = <endereço> da instrução decodificada
+    sw $v1, 8($t0)                               # armazena o <endereço> para a <Definição da Instrução> na instrução decodificada
+
+    beq $v1, $zero, decodeinst_return            # retorna caso uma definição válida não foi encontrado
+                                                 # isto é, se o endereço for nulo
+
+    move $a0, $t0                                # $a0 = <endereço> para a <Instrução Decodificada>
+    move $a1, $v0                                # $a1 = <endereço> para o vetor de <Campo>
+    jal extractops                               # extrai os operandos
+
+decodeinst_return:
+    lw $ra, 0($sp)                               # restaura o endereço de retorno
+    addiu $sp, $sp, 8                            # restaura a pilha
+    jr $ra                                       # retorna ao chamador
+
+
+# Isola e decodifica os operandos de acordo com o tipo e com a definição da instrução.
+#
+# argumentos:
+# $a0 : <endereço> para <Instrução Decodificada>
+# $a1 : <endereço> para <Tipo da Instrução> (vetor de <Campo>)
+#
+# pilha:
+# $sp + 24 : $s5
+# $sp + 20 : $s4
+# $sp + 16 : $s3
+# $sp + 12 : $s2
+# $sp + 8  : $s1
+# $sp + 4  : $s0
+# $sp + 0  : $ra
+extractops:
+    addiu $sp, $sp, -28                          # ajusta a pilha
+    sw $s5, 24($sp)                              # armazena na pilha o registrador $s5
+    sw $s4, 20($sp)                              # armazena na pilha o registrador $s4
+    sw $s3, 16($sp)                              # armazena na pilha o registrador $s3
+    sw $s2, 12($sp)                              # armazena na pilha o registrador $s2
+    sw $s1, 8($sp)                               # armazena na pilha o registrador $s1
+    sw $s0, 4($sp)                               # armazena na pilha o registrador $s0
+    sw $ra, 0($sp)                               # armazena na pilha o endereço de retorno
+
+    move $s0, $a0                                # $s0 = argumento com o <endereço> para <Instrução Decodificada>
+    move $s1, $a1                                # $s1 = argumento com o <endereço> para o início do vetor de <Campo>
+
+    lw $s2, 0($s0)                               # $s2 = carrega a posição da instrução decodificada
+    lw $s3, 4($s0)                               # $s3 = carrega o código de máquina da instrução decodificada
+    lw $s4, 8($s0)                               # $s4 = carrega o <endereço> para a <Definição da Instrução> da instrucão decodificada
+
+    move $s5, $zero                              # $s5 = número do operando a ser decodificado
+    
+    j extractops_for_condition                   # verifica se existes campos
+
+extractops_for_loop:
+    sll $t0, $s5, 3                              # $t0 = deslocamento no vetor de <Campo>
+    addu $t0, $t0, $s1                           # $t0 = endereço efetivo do <Campo> sendo isolado
+
+    move $a0, $s3                                # $a0 = código de máquina
+
+    lw $t1, 0($t0)                               # $t1 = máscara
+    and $a0, $a0, $t1                            # $a0 = campo isolado pelo código de máscara
+
+    lw $t1, 4($t0)                               # $t1 = deslocamento
+    srlv $a0, $a0, $t1                           # $a0 = campo deslocado para o início
+
+    sll $t0, $s5, 2                              # $t0 = deslocamento no vetor com os tipos de operandos
+    addu $t0, $t0, $s4                           # $t1 = endereço efetivo do tipo do operando em relação a <Definição da Instrução>
+
+    lw $a1, 0($t0)                               # $a1 = tipo do operando
+    move $a2, $s2                                # $a2 = posição (endereço) da instrução
+    jal parseop                                  # decodifica o operador
+
+    sll $t0, $s5, 3                              # $t0 = deslocamento no vetor de <Operando>
+    addiu $t0, $t0, 12                           # $t0 = deslocamento na <Instrução Decodificada>
+    addu $t0, $t0, $s0                           # $t0 = endereço efetivo do operando
+
+    sw $v0, 0($t0)                               # armazena o tipo do operando na instrução decodificada
+    sw $v1, 4($t0)                               # armazena o valor do operando na instruação decodificada
+
+extractops_for_increment:
+    addiu $s5, $s5, 1
+
+extractops_for_condition:
+    blt $s5, 4, extractops_for_loop              # se $s5 for menor que 4, continua o for
+
+    lw $s5, 24($sp)                              # restaura o registrador $s5
+    lw $s4, 20($sp)                              # restaura o registrador $s4
+    lw $s3, 16($sp)                              # restaura o registrador $s3
+    lw $s2, 12($sp)                              # restaura o registrador $s2
+    lw $s1, 8($sp)                               # restaura o registrador $s1
+    lw $s0, 4($sp)                               # restaura o registrador $s0
+    lw $ra, 0($sp)                               # restaura o endereço de retorno
+    addiu $sp, $sp, 28                           # restaura a pilha
+
+    jr $ra                                       # retorna ao chamador
+    
+# Aplica operações para ajustar o valor do campo ao valor do operando em assembly.
+#
+# argumentos:
+# $a0 : <word> valor do campo
+# $a1 : <word> tipo do operando
+# $a2 : <word> posição do código de máquina
+#
+# saída:
+# $v0 : <word> tipo do operando
+# $v1 : <word> valor do operando
+parseop:
+    move $v1, $a0
+    move $v0, $a1
+    
+    jr $ra
--- trabalhos/t1/instdefs.asm
+++ trabalhos/t1/instdefs.asm
@@ -1,98 +1,99 @@
 .include "constants.asm"
 
-# Estrutura "Campo"
+# struct <Campo>
 # 8 bytes 
 #
-# Offset 0  <- mascara (word)
-# Offset 4  <- deslocamento (word)
+# Offset 0  : <word> mascara
+# Offset 4  : <word> deslocamento
 
-# Estrutura "Tipo de Instrução"
+# struct <Tipo da Instrução>
 # 32 bytes
 #
-# Offset 0  <- Campo 0
-# Offset 8  <- Campo 1
-# Offset 16 <- Campo 2
-# Offset 24 <- Campo 3
+# Offset 0  : <Campo> 0
+# Offset 8  : <Campo> 1
+# Offset 16 : <Campo> 2
+# Offset 24 : <Campo> 3
 
-# Estrutura "Definição da Instrução"
+# struct <Definição da Instrução>
 # 20 bytes
 #
-# Offset 0  <- tipo do campo 0
-# Offset 4  <- tipo do campo 1
-# Offset 8  <- tipo do campo 2
-# Offset 12 <- tipo do campo 3
-# Offset 16 <- endereço do formato
-
+# Offset 0  : <word> tipo do operando 0
+# Offset 4  : <word> tipo do operando 1
+# Offset 8  : <word> tipo do operando 2
+# Offset 12 : <word> tipo do operando 3
+# Offset 16 : <endereço> string de formato
 
 .text
+# Encontra o tipo (I, R ou J) e a definição da instrução que o código se refere.
+# 
 # argumentos
-# $a0 <- código da instrução
+# $a0 : <word> código
 #
-# saída
-# $v0 <- endereço para o tipo da instrução
-# $v1 <- endereço para a definição da instrução
+# retorno
+# $v0 : <endereço> para <Tipo da Instrução>
+# $v1 : <endereço> para <Definição da Instrução>
 .globl lookupinst
 lookupinst:
-    srl $t0, $a0, 26                            # i  ) $t0 <- campo OPCODE deslocando a instrução
-    andi $t1, $a0, 0x0000003F                   # ii ) $t1 <- campo FUNC isolando por uma máscara
+    srl $t0, $a0, 26                            # i  ) $t0 = campo OPCODE 
+    andi $t1, $a0, 0x0000003F                   # ii ) $t1 = campo FUNC
 
-    srl $t2, $a0, 16                            # iii) $t2 <- campo RT deslocado para o início
-    andi $t2, $t2, 0x0000002F                   #      $t2 <- campo RT isolado por uma máscara
+    srl $t2, $a0, 16                            # iii) $t2 = campo RT (deslocado para início)
+    andi $t2, $t2, 0x0000002F                   #      $t2 = campo RT (isolado por uma máscara)
     
-    beq $t0, $zero, lookupinst_op_00            # se OPCODE = 0x00, vá para lookupinst_op_00
-    beq $t0, 0x1c, lookupinst_op_1c             # se OPCODE = 0x1c, vá para lookupinst_op_1c
-    beq $t0, 0x01, lookupinst_op_01             # se OPCODE = 0x01, vá para lookupinst_op_01
+    beq $t0, $zero, lookupinst_op_00            # se campo OPCODE = 0x00, busca na tabela especial 0x00
+    beq $t0, 0x1c, lookupinst_op_1c             # se campo OPCODE = 0x1c, busca na tabela especial 0x1c
+    beq $t0, 0x01, lookupinst_op_01             # se campo OPCODE = 0x01, busca na tabela especial 0x01
 
 lookupinst_op_default:
-    bne $t0, 0x02, lookupinst_if_not_jump       # se OPCODE != 0x02, define o tipo da instrução para I
-    bne $t0, 0x03, lookupinst_if_not_jump       # se OPCODE != 0x03, define o tipo da instrução para I
+    bne $t0, 0x02, lookupinst_if_not_jump       # se campo OPCODE != 0x02 ou se campo OPCODE != 0x03,
+    bne $t0, 0x03, lookupinst_if_not_jump       # definição o tipo da instrução para I
 
 lookupinst_if_jump:
-    la $v0, inst_type_j                          # $v0 <- endereço do tipo J
-    j lookupinst_end_if_jump                     # vá para fora do bloco if
+    la $v0, inst_type_j                          # $v0 = endereço para do tipo J
+    j lookupinst_end_if_jump                     # salta para fora do bloco if
 
 lookupinst_if_not_jump:
-    la $v0, inst_type_i                          # $v0 <- endereço do tipo I
+    la $v0, inst_type_i                          # $v0 = endereço para do tipo I
 
 lookupinst_end_if_jump:
-    la $t3, root_insts                           # $t3 <- endereço base da tabela raíz
-    sll $t4, $t0, 2                              # $t4 <- deslocamento na tabela utlizando campo OPCODE
-    add $t4, $t4, $t3                            # $t4 <- endereço efetivo na tabela raíz
+    la $t3, root_insts                           # $t3 = endereço base da tabela raíz
+    sll $t4, $t0, 2                              # $t4 = deslocamento na tabela utilizando o campo OPCODE
+    add $t4, $t4, $t3                            # $t4 = endereço efetivo na tabela raíz
     
-    lw $v1, 0($t4)                               # $v1 <- endereço da definição da instrução
+    lw $v1, 0($t4)                               # $v1 = endereço da definição da instrução
 
     jr $ra                                       # retorna ao chamador
 
 lookupinst_op_00:
-    la $v0, inst_type_r                          # $v0 <- endereço do tipo R
+    la $v0, inst_type_r                          # $v0 = endereço do tipo R
 
-    la $t3, special_00_insts                     # $t3 <- endereço base da tabela especial 0x00
-    sll $t4, $t1, 2                              # $t4 <- deslocamento na tabela utilizando o campo FUNC
-    addu $t4, $t4, $t3                           # $t4 <- endereço efetivo na tabela especial 0x00
+    la $t3, special_00_insts                     # $t3 = endereço base da tabela especial 0x00
+    sll $t4, $t1, 2                              # $t4 = deslocamento na tabela utilizando o campo FUNCT
+    addu $t4, $t4, $t3                           # $t4 = endereço efetivo na tabela especial 0x00
     
-    lw $v1, 0($t4)                               # $v1 <- endereço da definição da instrução
+    lw $v1, 0($t4)                               # $v1 = endereço da definição da instrução
 
     jr $ra                                       # retorna ao chamador
 
 lookupinst_op_1c:
-    la $v0, inst_type_r                          # $v0 <- endereço do tipo R
+    la $v0, inst_type_r                          # $v0 = endereço para o tipo R
 
-    la $t3, special_1c_insts                     # $t3 <- endereço base da tabela especial 0x1c
-    sll $t4, $t1, 2                              # $t4 <- deslocamento na tabela utilizando campo FUNC
-    addu $t4, $t4, $t3                           # $t4 <- endereço efetivo na tabela especial 0x1c
+    la $t3, special_1c_insts                     # $t3 = endereço base da tabela especial 0x1c
+    sll $t4, $t1, 2                              # $t4 = deslocamento na tabela utilizando o campo FUNCT
+    addu $t4, $t4, $t3                           # $t4 = endereço efetivo na tabela especial 0x1c
     
-    lw $v1, 0($t4)                               # $v1 <- endereço da definição da instrução
+    lw $v1, 0($t4)                               # $v1 = endereço da definição da instrução
 
     jr $ra                                       # retorna ao chamador
 
 lookupinst_op_01:
-    la $v0, inst_type_i                          # $v0 <- endereço do tipo I
+    la $v0, inst_type_i                          # $v0 = endereço do tipo I
 
-    la $t3, special_rt_insts                     # $t3 <- endereço base da tabela especial RT
-    sll $t4, $t2, 2                              # $t4 <- deslocamento na tabela utilizando o campo RT
-    addu $t4, $t4, $t3                           # $t4 <- endereço efetivo na tabela especial RT
+    la $t3, special_rt_insts                     # $t3 = endereço base da tabela especial RT
+    sll $t4, $t2, 2                              # $t4 = deslocamento na tabela utilizando o campo RT
+    addu $t4, $t4, $t3                           # $t4 = endereço efetivo na tabela especial RT
     
-    lw $v1, 0($t4)                               # $v1 <- endereço da definição da instrução
+    lw $v1, 0($t4)                               # $v1 = endereço da definição da instrução
 
     jr $ra                                       # retorna ao chamador
     
@@ -138,7 +139,7 @@ inst_type_i:
 .word 0x0000FFFF # mascara      = 000000 00000 00000   1111111111111111
 .word 0          # deslocamento = 0
 # Campo Ignorado:
-.word 0          # mascara      = 000000 00000 00000 00000 00000 000000
+.word 0          # mascara      = 000000 00000 00000   0000000000000000
 .word 0          # deslocamento = 0
 
 
@@ -164,7 +165,8 @@ inst_type_j:
 .word 0          # deslocamento = 0
 
 
-# Tabela raíz de Definições de Instrução
+# Tabela raíz de <Definição da Instrução>
+# Acessada com o deslocamento pelo campo OPCODE
 root_insts:
     .word 0
     .word 0
@@ -232,7 +234,8 @@ root_insts:
     .word 0
     .word 0
 
-# Tabela especial definição de instruções com opcode 0x00
+# Tabela especial de <Definição da Instrução> com o campo OPCODE = 0x00
+# Acessada com o deslocamento pelo campo FUNCT
 special_00_insts:
     .word 0
     .word 0
@@ -300,7 +303,8 @@ special_00_insts:
     .word 0
     .word 0
 
-# Tabela especial definição de instruções com opcode 0x1c
+# Tabela especial de <Definição da Instrução> com o campo OPCODE = 0x1C
+# Acessada com o deslocamento pelo campo FUNCT
 special_1c_insts:
     .word 0
     .word 0
@@ -367,7 +371,8 @@ special_1c_insts:
     .word 0
     .word 0
 
-# Tabela especial definição de instruções com opcode 0x01
+# Tabela especial de <Definição da Instrução> com o campo OPCODE = 0x01
+# Acessada com o deslocamento pelo campo RT
 special_rt_insts:
     .word 0
     .word 0
